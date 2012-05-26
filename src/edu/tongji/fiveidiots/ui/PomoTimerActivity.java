55cc03f7160810838398c634bce1e8b017ec40b4
Andriy Lin <lxk19921205@gmail.com>
2012-05-26 14:03:13 +0800

    可以将计时器设置为全屏了

--- src/edu/tongji/fiveidiots/ui/PomoTimerActivity.java
+++ src/edu/tongji/fiveidiots/ui/PomoTimerActivity.java
@@ -3,21 +3,27 @@ package edu.tongji.fiveidiots.ui;
 import edu.tongji.fiveidiots.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * 番茄计时器
+ * 
  * @author Andriy
- *
  */
 public class PomoTimerActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.timer);
-		
-		//
-	}
 
+		//设置全屏
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.timer);
+		//TODO
+	}
 
 }
