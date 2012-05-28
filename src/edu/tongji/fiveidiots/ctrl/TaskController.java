8d68f2045f07021b3830478cb08fc7f37af55534
qrc <qiurc90@gmail.com>
2012-05-28 22:48:55 +0800

    生成ctrl包中的TaskInfo.java和TaskController.java

--- src/edu/tongji/fiveidiots/ctrl/TaskController.java
+++ src/edu/tongji/fiveidiots/ctrl/TaskController.java
@@ -4,6 +4,114 @@
  */
 package edu.tongji.fiveidiots.ctrl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.R.integer;
+import android.R.layout;
+
 public class TaskController {
+	
+	private ArrayList<TaskInfo> taskContainer;
+	private TaskInfo tempTask;
+	static int oneclock = 25;
+	
+	TaskController(){
+		taskContainer = new ArrayList<TaskInfo>();
+		taskContainer.clear();
+	}
+	public void AddTask(TaskInfo aTask){
+		taskContainer.add(aTask);
+	}
+	public void RemoveTask(int id){
+		for ( int i = 0; i < taskContainer.size(); ++ i){
+			tempTask = taskContainer.get(i);
+			if (tempTask.getId() == id){
+				taskContainer.remove(i);
+				break;
+			}
+		}	
+	}
+	public TaskInfo ShowTaskInfo(int id){
+		for ( int i = 0; i < taskContainer.size(); ++ i){
+			tempTask = taskContainer.get(i);
+			if (tempTask.getId() == id){
+				return tempTask;
+			}
+		}
+		return null;
+	}
+	public Boolean ModifyTaskInfo(int id,TaskInfo aTask){
+		for ( int i = 0; i < taskContainer.size(); ++ i){
+			tempTask = taskContainer.get(i);
+			if (tempTask.getId() == id){
+				tempTask.copy(aTask);
+				return true;
+			}
+		}
+		return false;
+	}
+	public ArrayList<TaskInfo> ShowTaskList(){
+		return taskContainer;
+	}
+	
+	public ArrayList<TaskInfo> SearchTag(String str){
+		ArrayList<TaskInfo> tempContainer = new ArrayList<TaskInfo>();
+		tempContainer.clear();
+		for ( int i = 0; i < taskContainer.size(); ++ i){
+			tempTask = taskContainer.get(i);
+			if (tempTask.searchTag(str)){
+				tempContainer.add(tempTask);
+			}
+		}
+		return tempContainer;
+	}
+	
+	public int calculateTime(Date cur,Date des){
+		int month [] = {31,28,31,30,31,30,31,31,30,31,30,31};
+		return 200;
+	}
+	public TaskInfo Suggest(Date cur){
+		ArrayList<TaskInfo> tempContainer = new ArrayList<TaskInfo>();
+		tempContainer.clear();
+		double minfac = 10000.0;
+		int minpri = 10000;
+		TaskInfo ansFac = null;
+		TaskInfo ansPri = null;
+		for ( int i = 0; i < taskContainer.size(); ++ i){
+			tempTask = taskContainer.get(i);
+			if (!tempTask.IsExpire() && !tempTask.IsFinish()){
+				int num = calculateTime(cur,tempTask.getDeadline());
+				int cycleleft = num / oneclock - tempTask.getNcycle();
+				if (cycleleft <= 0){
+					if (tempTask.getPri() < minpri){
+						ansPri = tempTask;
+						minpri = tempTask.getPri();
+					}
+				}
+				else if (tempTask.getPri() * 0.75 + cycleleft * 0.25 < minfac){
+					ansFac = tempTask;
+					minfac = tempTask.getPri() * 0.75 + cycleleft * 0.25;
+				}
+			}
+		}
+		if (minpri < 10000){
+			return ansPri;
+		}
+		else return ansFac;
+	}
+	
+	public void FinishCycle(int id,int interrupt,double percent,Date cur){
+		for ( int i = 0; i < taskContainer.size(); ++ i){
+			tempTask = taskContainer.get(i);
+			if (tempTask.getId() == id){
+				tempTask.FinishCycle(interrupt,percent,cur);
+				break;
+			}
+		}
+	}
+	
+	
 	
 }
--- src/edu/tongji/fiveidiots/ctrl/TaskInfo.java
+++ src/edu/tongji/fiveidiots/ctrl/TaskInfo.java
@@ -8,13 +8,70 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class TaskInfo {
-	private int pri,pre_id,next_id,cycle,way,interrupt;
+	private int pri,pre_id,next_id,pcycle,ncycle,way,interrupt,id;
 	private double percent;
 	private String name,addr,hint;
 	private Date starttime,deadline;
 	private ArrayList<String> tag;
-	private Boolean expire;
+	private Boolean expire,finish;
 	
+	TaskInfo(int id,String name, String addr, String hint, int pri, int pre_id, int next_id, int cycle, int way, Date deadline){
+		this.id = id;
+		this.name = name;
+		this.addr = addr;
+		this.hint = hint;
+		this.pri = pri;
+		this.pre_id = pre_id;
+		this.next_id = next_id;
+		this.pcycle = 0;
+		this.ncycle = cycle;
+		this.way = way;
+		this.deadline = deadline;
+		expire = false;
+		finish = false;
+		percent = 0.0;
+		tag = new ArrayList<String>();
+		tag.clear();
+	}
+	
+	public void copy(TaskInfo aTask){
+		this.id = aTask.getId();
+		this.name = aTask.getName();
+		this.addr = aTask.getAddr();
+		this.hint = aTask.getHint();
+		this.pri = aTask.getPri();
+		this.pre_id = aTask.getPre_id();
+		this.next_id = aTask.getNext_id();
+		this.pcycle = aTask.getPcycle();
+		this.ncycle = aTask.getNcycle();
+		this.way = aTask.getWay();
+		this.deadline = aTask.getDeadline();
+		this.percent = aTask.getPercent();
+		if (aTask.IsExpire()) this.expire = true;
+		else this.expire = false;
+		if (aTask.IsFinish()) this.finish = true;
+		else this.finish = false;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPcycle() {
+		return pcycle;
+	}
+	public void setPcycle(int pcycle) {
+		this.pcycle = pcycle;
+	}
+	public int getNcycle() {
+		return ncycle;
+	}
+	public void setNcycle(int ncycle) {
+		this.ncycle = ncycle;
+	}
 	public int getPri() {
 		return pri;
 	}
@@ -33,12 +90,7 @@ public class TaskInfo {
 	public void setNext_id(int next_id) {
 		this.next_id = next_id;
 	}
-	public int getCycle() {
-		return cycle;
-	}
-	public void setCycle(int cycle) {
-		this.cycle = cycle;
-	}
+	
 	public int getWay() {
 		return way;
 	}
@@ -104,7 +156,37 @@ public class TaskInfo {
 	public void deleteTag(int id){
 		tag.remove(id);
 	}
+	public Boolean searchTag(String str){
+		for ( int i = 0; i < tag.size(); ++ i){
+			if (str.equals(tag.get(i))){
+				return true;
+			}
+		}
+		return false;
+	}
 	
-	public Boolean Is
+	public Boolean IsExpire(){
+		return expire;
+	}
+	public void SetExpire(){
+		expire = true;
+	}
+	
+	public Boolean IsFinish(){
+		return finish;
+	}
+	public void SetFinish(){
+		finish = true;
+	}
+	
+	public void FinishCycle(int interrupt,double percent,Date cur){
+		this.interrupt += interrupt;
+		this.percent += percent;
+		if (this.percent >= 100) finish = true;
+		if (this.deadline.before(cur)) expire = true;
+		pcycle ++;
+		ncycle = (int)Math.ceil((100.0 - percent) / (percent / pcycle));
+	}
+	
 	
 }
