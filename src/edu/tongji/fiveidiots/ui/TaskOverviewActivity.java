c5c1b0f311088fe666b0a329a3386f238fdca7e4
Rainbow Wan <maydaycoolwbw@gmail.com>
2012-05-29 23:43:32 +0800

    add actionbar item and quick action grid

--- src/edu/tongji/fiveidiots/ui/TaskOverviewActivity.java
+++ src/edu/tongji/fiveidiots/ui/TaskOverviewActivity.java
@@ -4,10 +4,21 @@ import edu.tongji.fiveidiots.R;
 import edu.tongji.fiveidiots.util.ActivityUtil;
 import greendroid.app.GDActivity;
 import greendroid.widget.ActionBar;
+import greendroid.widget.ActionBar.OnActionBarListener;
 import greendroid.widget.ActionBarItem;
 import greendroid.widget.ActionBarItem.Type;
+import greendroid.widget.QuickAction;
+import greendroid.widget.QuickActionGrid;
+import greendroid.widget.QuickActionWidget;
+import greendroid.widget.QuickActionWidget.OnQuickActionClickListener;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -16,6 +27,9 @@ import android.widget.Toast;
  */
 public class TaskOverviewActivity extends GDActivity {
 
+	private QuickActionGrid mGridMore;
+	private QuickActionGrid mGridTimeLine;
+	
 	public TaskOverviewActivity(){
 		super(ActionBar.Type.Normal);
 	}
@@ -24,21 +38,80 @@ public class TaskOverviewActivity extends GDActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setActionBarContentView(R.layout.idoit);
-        addActionBarItem(Type.Add);
-        addActionBarItem(Type.Edit);
+        
+        prepareActionBar();
+        prepareQuickActionGrid();
         
         Log.i("__ANDRIY__", "TaskOverview.onCreate()");
 	}
+	
+	private void prepareActionBar()
+	{
+		addActionBarItem(Type.Add, R.id.action_bar_add);
+        addActionBarItem(Type.Eye, R.id.action_bar_timer);
+        addActionBarItem(Type.List, R.id.action_bar_more);
+	}
+	
+	private void prepareQuickActionGrid()
+	{
+		mGridMore = new QuickActionGrid(this);
+		mGridMore.addQuickAction(new MyQuickAction(this, R.drawable.gd_action_bar_eye, R.string.exit));
+		mGridMore.addQuickAction(new MyQuickAction(this, R.drawable.gd_action_bar_eye, R.string.exit));
+		mGridMore.addQuickAction(new MyQuickAction(this, R.drawable.gd_action_bar_eye, R.string.exit));
+		mGridMore.setOnQuickActionClickListener(mQuickActionListener);
+	}
+	
+	private OnQuickActionClickListener mQuickActionListener = new OnQuickActionClickListener() {
+		
+		@Override
+		public void onQuickActionClicked(QuickActionWidget widget, int position) {
+			Toast.makeText(TaskOverviewActivity.this, "Item " + position + " clicked", Toast.LENGTH_SHORT).show();
+		}
+	};
 
 	@Override
 	public boolean onHandleActionBarItemClick(ActionBarItem item, int position) {
-		if (position == 0) {
-			//第一个添加的东西，目前是add，当做进入计时器
-			ActivityUtil.startNewActivity(this, PomotimerActivity.class, 0, false);
+		
+		//if press HOME actionbar item, the 'item' value is null, so catch it here
+		if(position == OnActionBarListener.HOME_ITEM)
+		{
+			return true;
 		}
-		else {
-			Toast.makeText(this, "bar_item at pos: " + position, Toast.LENGTH_SHORT).show();
+
+		switch(item.getItemId())
+		{
+		case R.id.action_bar_add:
+			
+			break;
+		case R.id.action_bar_timer:
+			ActivityUtil.startNewActivity(this, PomotimerActivity.class, 0, false);
+			break;
+		case R.id.action_bar_more:
+			onShowGrid(item.getItemView());
+			break;
+		default:
+			break;	
 		}
 		return true;
 	}
+	
+	public void onShowGrid(View v) {
+        mGridMore.show(v);
+    }
+	
+    private static class MyQuickAction extends QuickAction {
+        
+        private static final ColorFilter BLACK_CF = new LightingColorFilter(Color.BLACK, Color.BLACK);
+
+        public MyQuickAction(Context ctx, int drawableId, int titleId) {
+            super(ctx, buildDrawable(ctx, drawableId), titleId);
+        }
+        
+        private static Drawable buildDrawable(Context ctx, int drawableId) {
+            Drawable d = ctx.getResources().getDrawable(drawableId);
+            d.setColorFilter(BLACK_CF);
+            return d;
+        }
+        
+    }
 }
