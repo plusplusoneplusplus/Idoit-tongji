f0182e28ec973a6bb9f3628afea9c64b6c870081
Rainbow Wan <maydaycoolwbw@gmail.com>
2012-06-04 22:52:05 +0800

    修复推荐功能bug

--- src/edu/tongji/fiveidiots/ui/OverviewTaskListActivity.java
+++ src/edu/tongji/fiveidiots/ui/OverviewTaskListActivity.java
@@ -528,9 +528,14 @@ public class OverviewTaskListActivity extends OverviewTagListActivity{
 		case R.string.recommend:
 			Bundle bundle = new Bundle();
 			TaskInfo task = new TaskController(this).Suggest(new Date(), new Settings(this).getPomotimerDuration());
-			bundle.putLong(TASK_ID_STR, task.getId());
-			bundle.putString(TASK_NAME_STR, task.getName());
-			ActivityUtil.startActivityWithBundle(OverviewTaskListActivity.this, PomotimerActivity.class, 0, false, bundle);
+			if (task == null) {
+				Toast.makeText(this, "暂无推荐任务", Toast.LENGTH_SHORT).show();
+			}
+			else {
+				bundle.putLong(TASK_ID_STR, task.getId());
+				bundle.putString(TASK_NAME_STR, task.getName());
+				ActivityUtil.startActivityWithBundle(OverviewTaskListActivity.this, PomotimerActivity.class, 0, false, bundle);
+			}
 			break;
 		case R.string.settings:
 			ActivityUtil.startNewActivity(OverviewTaskListActivity.this, SettingsActivity.class, 0L, false);
